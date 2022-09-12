47fa3d343f69bd0c859c0ece3ec89808a6235028
Samy Fodil <fodil.samy@gmail.com>
2022-09-12 01:23:52 -0500

    fixed submodule

--- taubyte/build.sh
+++ taubyte/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cd /src && git submodule init && git submodule update && hugo && mv public out
+
+cd /src && git submodule init && git submodule update && hugo && (mkdir -pv src/out || true) && mv public src/out
+
+
+ls src/out
 
 exit $?
